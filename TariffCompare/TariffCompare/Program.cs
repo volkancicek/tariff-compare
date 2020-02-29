5b2e9d4ad1bbdd0691bba798d288fa76ddc8be58
volkancicek <volkanericlice@gmail.com>
2020-02-29 17:01:01 +0100

    update start up

--- TariffCompare/TariffCompare/Program.cs
+++ TariffCompare/TariffCompare/Program.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +10,8 @@ namespace TariffCompare
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- TariffCompare/TariffCompare/Startup.cs
+++ TariffCompare/TariffCompare/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using TariffCompare.Services;
 
 namespace TariffCompare
 {
@@ -26,6 +20,7 @@ namespace TariffCompare
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddTransient<ITariffService, TariffService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
